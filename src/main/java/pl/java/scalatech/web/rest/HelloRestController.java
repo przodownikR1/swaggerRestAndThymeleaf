4f42738a1c7c264f93d79ecd212689514a88aef8
Slawomir Borowiec <przodownik@tlen.pl>
2015-07-20 16:17:20 +0200

    @Api test

--- src/main/java/pl/java/scalatech/web/rest/HelloRestController.java
+++ src/main/java/pl/java/scalatech/web/rest/HelloRestController.java
@@ -10,12 +10,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
 
+@Api(value = "Test controller", description = "API test ")
 @RestController
 public class HelloRestController {
     @RequestMapping("/api/hello")
-    @ApiOperation("hello world")
+    @ApiOperation(value = "find person", httpMethod = "GET")
     public Person getPerson() {
         return new Person("przodownik", BigDecimal.valueOf(123));
     }
